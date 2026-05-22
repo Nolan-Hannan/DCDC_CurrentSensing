cd5ec63d3d6b6c57b3268ff2d8787138a3854550
Nolan-Hannan <nolhans28@gmail.com>
2026-05-21 21:25:20 -0500

    Add timers

---
+++ Inc/timers.h
@@ -0,0 +1,9 @@
+#ifndef TIMERS_H
+#define TIMERS_H
+
+uint8_t SysTimer_250ms(void);
+uint8_t SysTimer_50ms(void);
+uint8_t SysTimer_10ms(void);
+
+
+#endif
---
+++ Src/timers.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include "timers.h"
+
+uint8_t SysTimer_250ms(void)
+{
+    static uint32_t last = 0;
+    uint32_t now = HAL_GetTick();
+
+    if ((now - last) >= 250)
+    {
+        last += 250;
+        return TRUE;
+    }
+
+    return FALSE;
+}
+
+uint8_t SysTimer_50ms(void)
+{
+    static uint32_t last = 0;
+    uint32_t now = HAL_GetTick();
+
+    if ((now - last) >= 50)
+    {
+        last += 50;
+        return TRUE;
+    }
+
+    return FALSE;
+}
+
+uint8_t SysTimer_10ms(void)
+{
+    static uint32_t last = 0;
+    uint32_t now = HAL_GetTick();
+
+    if ((now - last) >= 10)
+    {
+        last += 10;
+        return TRUE;
+    }
+
+    return FALSE;
+}
